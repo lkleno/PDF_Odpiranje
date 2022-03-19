5e638dd79d9e94360caa90c95c50ff5edebfa23b
lkleno <lk5669@student.uni-lj.si>
2022-03-19 21:52:44 +0100

    Add project files.

---
+++ PDF_Odpiranje/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+namespace PDF_Odpiranje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = ConfigurationManager.AppSettings["filename"];
+            string[] fileGroup = Directory.GetFiles(filename, "*_R.pdf");
+            foreach (var x in fileGroup)
+            {
+                var newName = x.Substring(0, x.Length - 6) + ".pdf";
+                System.IO.File.Move(x, newName);
+                System.Diagnostics.Process.Start(newName);
+            }
+        }
+    }
+}
